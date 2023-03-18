433528f11522095bad12aa5f41bd26d2571f1aa2
omozzo <ritchieomozzo@gmail.com>
2023-03-17 17:32:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - A program that prints a line using a function printf
- * Return: 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
